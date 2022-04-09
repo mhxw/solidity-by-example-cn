b65781761937f7490a30576211d24d3ebc5022e8
MHXW <32643286+mhxw@users.noreply.github.com>
2022-04-10 00:12:24 +0800

    update style and git scripts

--- tool/push.sh
+++ tool/push.sh
@@ -4,6 +4,7 @@ git config user.name "MHXW"
 git config user.email "32643286+mhxw@users.noreply.github.com"
 git add .
 git commit -m 'first commit'
-git branch -M gh-pages
+git branch -M dev
+git remote rm origin
 git remote add origin https://github.com/mhxw/solidity-by-example-cn.git
-git push -u -f origin gh-pages
+git push -u -f dev
